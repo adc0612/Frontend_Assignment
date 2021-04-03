d88ae1be97590b66b06a3a0dba691d57161deb5f
Dohu <adc0612@gmail.com>
2021-04-03 20:23:37 +0900

    project init

---
+++ src/app.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  state = {
+    QuestionList: [],
+  };
+
+  render() {
+    return (
+      <div>
+        <h1>hello</h1>
+      </div>
+    );
+  }
+}
+
+export default App;
